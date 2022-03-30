4ac2fef7dbd7e6cf48550bf50b86004629441a0d
hackgit12 <98569431+hackgit12@users.noreply.github.com>
2022-03-30 10:07:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -407,7 +407,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ɪɴ yᴏᴜʀ ɢʀᴏᴜᴩ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🕹️ɢʀᴏᴜᴩ', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('🕹️ɢʀᴏᴜᴩ', url='https://t.me/FILIMPIRATESGROUP')
         ], [
             InlineKeyboardButton('ʜᴇʟᴩ💤', callback_data='help'),
             InlineKeyboardButton('☠️ᴀʙᴏᴜᴛ', callback_data='about')
