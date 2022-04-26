5b56144963b970508158ba04a04b98800d72b887
hackgit12 <98569431+hackgit12@users.noreply.github.com>
2022-04-26 16:05:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,10 +1,9 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>
 ɪᴛ'ꜱ ᴠᴇʀy ᴇᴀꜱy ᴊᴜꜱᴛ ᴀᴅᴅ ᴍᴇ ɪɴ yᴏᴜʀ ɢʀᴏᴜᴩ ᴀɴᴅ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ,
-ᴛʜᴀᴛꜱ ᴀʟʟ ɪ'ʟʟ ᴩʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇꜱ ᴛʜᴇʀᴇ 😈
+ᴛʜᴀᴛꜱ ᴀʟʟ ɪ'ʟʟ ᴩʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇꜱ ᴛʜᴇʀᴇ 😇
 """
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
+    HELP_TXT = """•"""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
 ✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/The_user_death>ᴩʀᴏꜰᴇꜱᴇʀ</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
@@ -13,47 +12,78 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
--  <a href=https://t.me/{}>{}</a> is a open source project. 
-- Source - sorry da tharan pattuella..... 
+- This bot is not a open source project. 
 
 <b>DEVS:</b>
 - <a href=https://t.me/The_user_death>ᴩʀᴏꜰᴇꜱᴇʀ</a>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
+    MANUELFILTER_TXT = """ꜰɪʟᴛᴇʀꜱ
 
 - Filter is the feature were users can set automated replies for a particular keyword and <a href=https://t.me/{}>{}</a> will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. <a href=https://t.me/{}>{}</a> should have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
+- ᴛʜɪꜱ ʙᴏᴛ should have admin privillage.
+- only admins can add filters in a chat.
+- alert buttons have a limit of 64 characters.
 
 <b>Commands and Usage:</b>
 • /filter - <code>add a filter in chat</code>
 • /filters - <code>list all the filters of a chat</code>
 • /del - <code>delete a specific filter in chat</code>
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    BUTTON_TXT = """Help: <b>Buttons</b>
+    BUTTON_TXT = """Buttons
 
-- <a href=https://t.me/{}>{}</a> Supports both url and alert inline buttons.
+- ᴛʜɪꜱ ʙᴏᴛ  Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. <a href=https://t.me/{}>{}</a> supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+- Telegram will not allows you to send buttons without any content, so content is mandatory.
+- ᴛʜɪꜱ ʙᴏᴛ  supports buttons with any telegram media type.
+- Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
 <code>[Button Text](buttonurl:https://t.me/FILIMPIRATESGROUP)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
+    AUTOFILTER_TXT = """Auto Filter
 
 <b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
+- Make me the admin of your channel if it's private.
+- make sure that your channel does not contains camrips, porn and fake files.
+- Forward the last message to me with quotes.
  I'll add all the files in that channel to my db."""
-    CONNECTION_TXT = """Help: <b>Connections</b>
+    BATCH_TXT = """
+𝙵𝙸𝙻𝙴 𝚂𝚃𝙾𝚁𝙴 𝙼𝙾𝙳𝚄𝙻𝙴../
+
+
+𝙱𝚈 𝚄𝚂𝙸𝙽𝙶 𝚃𝙷𝙸𝚂 𝙼𝙾𝙳𝚄𝙻𝙴 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚂𝚃𝙾𝚁𝙴 𝙵𝙸𝙻𝙴𝚂 𝙸𝙽 𝙼𝚈 𝙳𝙰𝚃𝙰𝙱𝙰𝚂𝙴 𝙰𝙽𝙳 𝙸 𝚆𝙸𝙻𝙻 𝙶𝙸𝚅𝙴 𝚈𝙾𝚄 𝙰 𝙿𝙴𝚁𝙼𝙰𝙽𝙴𝙽𝚃 𝙻𝙸𝙽𝙺 𝚃𝙾 𝙰𝙲𝙲𝙴𝚂𝚂 𝚃𝙷𝙴 𝚂𝙰𝚅𝙴𝙳 𝙵𝙸𝙻𝙴𝚂.𝙸𝙵 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃 𝚃𝙾 𝙰𝙳𝙳 𝙵𝙸𝙻𝙴𝚂 𝙵𝚁𝙾𝙼 𝙰 𝙿𝚄𝙱𝙻𝙸𝙲 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝚂𝙴𝙽𝙳 𝚃𝙷𝙴 𝙵𝙸𝙻𝙴 𝙻𝙸𝙽𝙺 𝙾𝙽𝙻𝚈 𝙾𝚁 𝚈𝙾𝚄 𝚆𝙰𝙽𝚃 𝚃𝙾 𝙰𝙳𝙳 𝙵𝙸𝙻𝙴𝚂 𝙵𝚁𝙾𝙼 𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝚈𝙾𝚄 𝙼𝚄𝚂𝚃 𝙼𝙰𝙺𝙴 𝙼𝙴 𝚃𝙷𝙴 𝙰𝙳𝙼𝙸𝙽 𝙸𝙽 𝚃𝙷𝙴𝙸𝚁 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𝚃𝙾 𝙰𝙲𝙲𝙴𝚂𝚂 𝚃𝙷𝙴 𝙵𝙸𝙻𝙴𝚂..../
+
+⪼ 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙰𝙽𝙳 𝚄𝚂𝙰𝙶𝙴 ›
+
+✯ /plink ›› 𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰𝙽𝚈 𝙼𝙴𝙳𝙸𝙰 𝚃𝙾 𝙶𝙴𝚃 𝙻𝙸𝙽𝙺.
+✯ /pbatch ›› 𝚄𝚂𝙴 𝚈𝙾𝚄𝚁 𝙼𝙴𝙳𝙸𝙰 𝙻𝙸𝙽𝙺 𝚆𝙸𝚃𝙷 𝚃𝙷𝙸𝚂 𝙲𝙾𝙼𝙼𝙰𝙽𝙳.
+✯ /batch ›› 𝚃𝙾 𝙲𝚁𝙴𝙰𝚃𝙴 𝙻𝙸𝙽𝙺 𝙵𝙾𝚁 𝙼𝚄𝙻𝚃𝙸𝙿𝙻𝙴 𝙿𝙾𝚂𝚃.
+
+✎ 𝐄𝐱𝐚𝐦𝐩𝐥𝐞 ›
+
+/batch https://t.me/c/1749754594/332 https://t.me/c/1749754594/336"""
+    ALIVE_TXT = """
+ALIVE MODULE
+• /alive - check me alive or dead🤧
+Just for a rasam😂"""
+    INFO_TXT = """
+Information
+
+Get information about something!
+
+Commands and Usage:
+• /id - get id of a specifed user.
+• /info  - get information about a user.
+
+NOTE:
+• IMDb should have admin privillage.
+• These commands works on both pm and group.
+• These commands can be used by any group member."""
+    CONNECTION_TXT = """ᴄᴏɴɴᴇᴄᴛɪᴏɴ
 
 - Used to connect bot to PM for managing filters 
 - it helps to avoid spamming in groups.
@@ -66,7 +96,7 @@ class script(object):
 • /connect  - <code>connect a particular chat to your PM</code>
 • /disconnect  - <code>disconnect from a chat</code>
 • /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
+    EXTRAMOD_TXT = """ɪɴꜰᴏ
 
 <b>NOTE:</b>
 these are the extra features of <a href=https://t.me/{}>{}</a>
@@ -76,7 +106,7 @@ these are the extra features of <a href=https://t.me/{}>{}</a>
 • /info  - <code>get information about a user.</code>
 • /imdb  - <code>get the film information from IMDb source.</code>
 • /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
+    ADMIN_TXT = """Admin ᴍᴏᴅ
 
 <b>NOTE:</b>
 This module only works for my admins
